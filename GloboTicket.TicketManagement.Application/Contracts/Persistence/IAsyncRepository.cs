c18870bbd191b27b1930e90cf54e632f07df9d97
Antony HM <antonio_hm25@hotmail.com>
2023-10-22 17:30:45 -0600

    agregando el proyecto Application

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace GloboTicket.TicketManagement.Application.Contracts.Persistence;
+
+public interface IAsyncRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(string Id);
+
+    Task<IReadOnlyList<T>> ListAllAsync();
+
+    Task<T> AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeleteAsync(T entity);
+}
